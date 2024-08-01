03b26f5ff197197b2b8f15e734c770de2c605ab8
Patrick Owusu Bofah <123265853+Patrick4350@users.noreply.github.com>
2024-08-01 11:46:26 +0000

    CreatePost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,35 +1,40 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { previous } from '../assets';
+import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
-    const navigate = useNavigate();
-    const [form, setForm] = useState({
-        name: '',
-        prompt: '',
-        photo: '',
-    });
+  const navigate = useNavigate();
 
-    const[generatingImg, setGeneratingImg] = useState(false);
-    const[loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
 
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <section className="max-w-7xl mx-auto">
-            <div>
-                <h1 className="font-extrabold text-[#222328] text-[32px]">
-                    Create
-                </h1>
-                <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
-                    Unleash your creativity with VisionCraft and share your breathtaking, imaginative images with our vibrant community.
-                </p>
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+        <div>
+            <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+            <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Unleash your creativity with VisionCraft and share your breathtaking, imaginative images with our vibrant community.</p>
+        </div>
+        <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-5">
+
             </div>
+        </form>
+     
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default CreatePost
+export default CreatePost;
