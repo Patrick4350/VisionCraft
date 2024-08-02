acb0fa2748ef83e6eb627779c77dd225caaeb7da
Patrick Owusu Bofah <123265853+Patrick4350@users.noreply.github.com>
2024-08-02 09:10:51 +0000

    Image fetch error

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
     return (
-        
-        <div> 
+        <div>
             <div className="flex items-center gap-2 mb-2">
-               <label
+                <label
                     htmlFor={name}
                     className="block text-sm font-medium text-gray-900"
-               >
+                >
                     {labelName}
                     {isSurpriseMe && (
                         <button
@@ -19,11 +18,12 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
                             Surprise me
                         </button>
                     )}
-                </label> 
+                </label>
             </div>
             <input 
                 type={type}
                 id={name}
+                name={name}
                 placeholder={placeholder}
                 value={value}
                 onChange={handleChange}
@@ -31,7 +31,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
             />
         </div>
-    )
+    );
 }
 
-export default FormField
+export default FormField;
